ba57ff2d820a54c36a0ffff63fa2e3e402b41e18
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2022-02-23 13:43:26 +0100

    build of setup .exe

---
+++ create_windows_installer.sh
@@ -0,0 +1,10 @@
+unzip -o package.zip
+docker rm setup
+docker container create --name setup amake/innosetup setup.iss
+docker cp ./package setup:/work/
+docker cp setup.iss setup:/work/
+docker cp LICENSE.md setup:/work/
+docker start -i -a setup
+docker cp setup:/work/Output/. .
+docker rm setup
+rm -r package
